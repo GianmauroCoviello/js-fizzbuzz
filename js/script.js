3593e5e8e2f9fc21b0cb005239de4ad11ae1b35f
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-22 09:51:35 +0200

    [DOCS] Correzione effettuata.

--- js/script.js
+++ js/script.js
@@ -5,20 +5,20 @@ let i;
 for (i = 1; i <=100; i++) {
 
 
-// PRIMA CONDIZIONE PER I MULTIPLI DI 3
-    if (i % 3 ==0 ) {
-        console.log('fizz')
-
-        // PRIMA CONDIZIONE PER I MULTIPLI SIA DI 3 CHE DI 5
-        if (i % 5 == 0) {
-            console.log('fizzbuzz');
-            
-        }
+// PRIMA CONDIZIONE PER I MULTIPLI SIA DI 3 CHE DI 5
+    if (i % 3 ==0 && i % 5 == 0 ) {
+        console.log('fizzbuzz')
+   
     // PRIMA CONDIZIONE PER I MULTIPLI DI 5    
     } else if (i % 5 ==0) {
         console.log('buzz');
+
+    // PRIMA CONDIZIONE PER I MULTIPLI DI 3       
+    } else if (i % 3 ==0) {
+        console.log('fizz');
         
     }
+    
     
     else{    
         console.log(i);
